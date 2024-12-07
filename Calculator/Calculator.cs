5672e803a11d0cd53eeeca7f74187acef74f7d00
Дмитрий Квасов <dimonkwasoff@gmail.com>
2024-12-07 03:08:21 +0300

    Написал код для кнопки /

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -126,7 +126,28 @@ namespace Calculator
 
         private void divisionButton_Click(object sender, EventArgs e)
         {
-            resultWindow.Text += "/";
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(resultWindow.Text))
+                {
+                    if (resultWindow.Text[^1] == '+' || resultWindow.Text[^1] == '-' || resultWindow.Text[^1] == '*' || resultWindow.Text[^1] == '/')
+                    {
+                        if (resultWindow.Text[^2] == '+' || resultWindow.Text[^2] == '-' || resultWindow.Text[^2] == '*' || resultWindow.Text[^2] == '/')
+                        {
+                            resultWindow.Text = resultWindow.Text[..^2].ToString() + "/";
+                        }
+                        else
+                        {
+                            resultWindow.Text = resultWindow.Text[..^1].ToString() + "/";
+                        }
+                    }
+                    else
+                    {
+                        resultWindow.Text += "/";
+                    }
+                }
+            }
+            catch { }
         }
     }
 }
